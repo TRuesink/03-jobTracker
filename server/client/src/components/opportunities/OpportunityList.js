dc41433f4bf253765c9727fcb56c3c94f06b7d0e
Tim Ruesink <timothy.ruesink@gmail.com>
2021-02-25 06:56:59 -0600

    added stage change function to opp list

--- server/client/src/components/opportunities/OpportunityList.js
+++ server/client/src/components/opportunities/OpportunityList.js
@@ -2,49 +2,111 @@ import React from "react";
 import _ from "lodash";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { Form } from "semantic-ui-react";
 import { fetchOpportunities, editOpportunity } from "../../actions";
 
 import CreateOpportunity from "./CreateOpportunity";
+
+const stageOptions = [
+  {
+    key: "research",
+    text: "research",
+    value: "research",
+  },
+  {
+    key: "info meeting",
+    text: "info meeting",
+    value: "info meeting",
+  },
+  {
+    key: "screening interview",
+    text: "screening interview",
+    value: "screening interview",
+  },
+  {
+    key: "technical interview",
+    text: "technical interview",
+    value: "technical interview",
+  },
+  {
+    key: "negotiation",
+    text: "negotiation",
+    value: "negotiation",
+  },
+  {
+    key: "won",
+    text: "won",
+    value: "won",
+  },
+  {
+    key: "lost",
+    text: "lost",
+    value: "lost",
+  },
+];
 
 class OpportunityList extends React.Component {
   componentDidMount() {
     this.props.fetchOpportunities();
   }
+
+  state = { editMode: false };
+
+  renderDropdown({ input, options }) {
+    return (
+      <Form.Select
+        {...input}
+        fluid
+        selection
+        options={options}
+        onChange={(e, { value }) => input.onChange(value)}
+      />
+    );
+  }
+
   renderOpportunities() {
-    return this.props.opportunities.map((opp) => {
-      const colorClass = opp.active ? "positive" : "negative";
-      const statusClass = this.props.inProgress ? "disabled" : "";
-      return (
-        <tr className={colorClass + " " + statusClass}>
-          <td>
-            <Link to={`/opportunities/${opp._id}`}>{opp.name}</Link>
-          </td>
-          <td>{opp.stage}</td>
-          <td>{"test"}</td>
-          <td className="collapsing">
-            {opp.active ? (
-              <button
-                className="ui button"
-                onClick={() =>
-                  this.props.editOpportunity(opp._id, { active: false })
+    return this.props.opportunities
+      .sort((a, b) => {
+        if (a.stage === "lost" && b.stage !== "lost") {
+          return 1;
+        } else if (a.stage !== "lost" && b.stage === "lost") {
+          return -1;
+        } else {
+          return 0;
+        }
+      })
+      .map((opp) => {
+        const colorClass = opp.stage !== "lost" ? "positive" : "negative";
+        const statusClass = this.props.inProgress ? "disabled" : "";
+        const lastActivity = opp.activities.sort(
+          (a, b) => Date.parse(a.createdAt) - Date.parse(b.createdAt)
+        )[0];
+        console.log(lastActivity);
+        return (
+          <tr key={opp._id} className={colorClass + " " + statusClass}>
+            <td>
+              <Link to={`/opportunities/${opp._id}`}>{opp.name}</Link>
+            </td>
+            <td>
+              <Form.Select
+                selection
+                options={stageOptions}
+                onChange={(e, { value }) =>
+                  this.props.editOpportunity(opp._id, { stage: value })
                 }
-              >
-                Deactivate
-              </button>
-            ) : (
-              <button
-                className="ui small button"
-                onClick={() =>
-                  this.props.editOpportunity(opp._id, { active: true })
-                }
-              >
-                Activate
-              </button>
-            )}
-          </td>
-        </tr>
-      );
-    });
+                defaultValue={opp.stage}
+                loading={false}
+                disabled={!this.state.editMode}
+              />
+            </td>
+            <td>
+              {lastActivity
+                ? new Date(lastActivity.createdAt).toLocaleDateString()
+                : "no activities"}
+            </td>
+          </tr>
+        );
+      });
   }
 
   render() {
@@ -60,14 +122,29 @@ class OpportunityList extends React.Component {
               <th>Name</th>
               <th>Stage</th>
               <th>Last Activity</th>
-              <th></th>
             </tr>
           </thead>
           <tbody>{this.renderOpportunities()}</tbody>
           <tfoot class="full-width">
             <tr>
-              <th></th>
-              <th colspan="4">
+              <th>
+                {this.state.editMode ? (
+                  <button
+                    onClick={() => this.setState({ editMode: false })}
+                    className="ui primary basic button"
+                  >
+                    Save
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => this.setState({ editMode: true })}
+                    className="ui primary basic button"
+                  >
+                    Edit
+                  </button>
+                )}
+              </th>
+              <th colSpan="4">
                 <CreateOpportunity />
               </th>
             </tr>
