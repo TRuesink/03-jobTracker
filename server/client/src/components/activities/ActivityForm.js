3862623ed74977e40ccb23d377a1b81792d539ba
Tim Ruesink <timothy.ruesink@gmail.com>
2021-03-09 07:58:07 -0600

    contact form validation

--- server/client/src/components/activities/ActivityForm.js
+++ server/client/src/components/activities/ActivityForm.js
@@ -21,7 +21,7 @@ class ActivityForm extends React.Component {
     this.props.closeModal(false);
   };
 
-  renderDropdown({ input, options, disabled }) {
+  renderDropdown({ input, options, disabled, label, meta }) {
     const optionsArray = options.map((opt) => {
       return {
         key: opt._id || opt.purpose,
@@ -29,16 +29,51 @@ class ActivityForm extends React.Component {
         value: opt._id,
       };
     });
+    const errorClass = meta.error && meta.touched ? "error" : null;
     return (
-      <Form.Select
-        {...input}
-        fluid
-        selection
-        options={optionsArray}
-        onChange={(e, { value }) => input.onChange(value)}
-        disabled={disabled}
-        clearable={true}
-      />
+      <div className={`field ${errorClass}`}>
+        <label>{label}</label>
+        {options.length === 0 ? null : (
+          <Form.Select
+            {...input}
+            fluid
+            selection
+            options={optionsArray}
+            onChange={(e, { value }) => input.onChange(value)}
+            disabled={disabled}
+            clearable={true}
+          />
+        )}
+        {meta.error && meta.touched ? (
+          <span style={{ color: "#9F3A38" }}>{meta.error}</span>
+        ) : null}
+      </div>
+    );
+  }
+
+  renderInput({ input, meta, label, type }) {
+    const errorClass = meta.error && meta.touched ? "error" : null;
+    return (
+      <div className={`field ${errorClass}`}>
+        <label>{label}</label>
+        <input {...input} type={type}></input>
+        {meta.error && meta.touched ? (
+          <span style={{ color: "#9F3A38" }}>{meta.error}</span>
+        ) : null}
+      </div>
+    );
+  }
+
+  renderTextArea({ input, meta, label, rows }) {
+    const errorClass = meta.error && meta.touched ? "error" : null;
+    return (
+      <div className={`field ${errorClass}`}>
+        <label>{label}</label>
+        <textarea {...input} rows={rows}></textarea>
+        {meta.error && meta.touched ? (
+          <span style={{ color: "#9F3A38" }}>{meta.error}</span>
+        ) : null}
+      </div>
     );
   }
 
@@ -49,54 +84,59 @@ class ActivityForm extends React.Component {
         onSubmit={this.props.handleSubmit(this.onFormSubmit)}
         className="ui form"
       >
-        <div className="field">
-          <label>Activity Date</label>
-          <Field name="createdAt" component="input" type="date" />
-        </div>
-        <div className="field">
-          <label>What oppportunity is this related to?</label>
-          {this.props.opportunities.length === 0 ? null : (
-            <Field
-              name="opportunity"
-              component={this.renderDropdown}
-              options={this.props.opportunities}
-              disabled={this.props.initialValues ? true : false}
-            />
-          )}
-        </div>
-        <div className="field">
-          <label>Contact (if applicable)</label>
-          {this.props.contacts.length === 0 ? null : (
-            <Field
-              name="contact"
-              component={this.renderDropdown}
-              options={this.props.contacts}
-            />
-          )}
-        </div>
-        <div className="field">
-          <label>Script (if applicable)</label>
-          {this.props.scripts.length === 0 ? (
-            <div>No Scripts</div>
-          ) : (
-            <Field
-              name="script"
-              component={this.renderDropdown}
-              options={this.props.scripts}
-            />
-          )}
-        </div>
-        <div className="field">
-          <label>What you did</label>
-          <Field name="description" component="textarea" rows="3" />
-        </div>
+        <Field
+          name="createdAt"
+          component={this.renderInput}
+          type="date"
+          label="Activity Date"
+        />
+        <Field
+          name="opportunity"
+          component={this.renderDropdown}
+          options={this.props.opportunities}
+          label="What opportunity is this related to?"
+          disabled={this.props.initialValues ? true : false}
+        />
+        <Field
+          name="contact"
+          component={this.renderDropdown}
+          options={this.props.contacts}
+          label="Contact (if applicable)"
+        />
+        <Field
+          name="script"
+          component={this.renderDropdown}
+          options={this.props.scripts}
+          label="Script (if applicable)"
+        />
+        <Field
+          name="description"
+          component={this.renderTextArea}
+          label="What you did"
+          rows="3"
+        />
       </form>
     );
   }
 }
 
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.createdAt) {
+    errors.createdAt = "Specify when activity was completed";
+  }
+  if (!formValues.opportunity) {
+    errors.opportunity = "Specify related opportunity";
+  }
+  if (!formValues.description) {
+    errors.description = "Describe what you did";
+  }
+  return errors;
+};
+
 ActivityForm = reduxForm({
   form: "activityForm",
+  validate: validate,
 })(ActivityForm);
 
 const mapStateToProps = (state) => {
--- server/client/src/components/contacts/ContactForm.js
+++ server/client/src/components/contacts/ContactForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
+import { Field, formValues, reduxForm } from "redux-form";
 import { connect } from "react-redux";
 import { fetchOpportunities } from "../../actions";
 import { Form } from "semantic-ui-react";
@@ -37,31 +37,62 @@ class ContactForm extends React.Component {
     this.props.closeModal(false);
   };
 
-  renderDropdown({ input, options, disabled }) {
+  renderDropdown({ input, options, disabled, label, meta }) {
     const optionsArray = options.map((opt) => {
       return { key: opt._id, text: opt.name, value: opt._id };
     });
+    const errorClass = meta.error && meta.touched ? "error" : null;
     return (
-      <Form.Select
-        {...input}
-        fluid
-        selection
-        options={optionsArray}
-        onChange={(e, { value }) => input.onChange(value)}
-        disabled={disabled}
-      />
+      <div className={`field ${errorClass}`}>
+        <label>{label}</label>
+        {options.length === 0 ? null : (
+          <Form.Select
+            {...input}
+            fluid
+            selection
+            options={optionsArray}
+            onChange={(e, { value }) => input.onChange(value)}
+            disabled={disabled}
+            clearable={true}
+          />
+        )}
+        {meta.error && meta.touched ? (
+          <span style={{ color: "#9F3A38" }}>{meta.error}</span>
+        ) : null}
+      </div>
     );
   }
 
-  renderRoleDropdown({ input, options }) {
+  renderRoleDropdown({ input, options, label, meta }) {
+    const errorClass = meta.error && meta.touched ? "error" : null;
     return (
-      <Form.Select
-        {...input}
-        fluid
-        selection
-        options={options}
-        onChange={(e, { value }) => input.onChange(value)}
-      />
+      <div className={`field ${errorClass}`}>
+        <label>{label}</label>
+        <Form.Select
+          {...input}
+          fluid
+          selection
+          options={options}
+          onChange={(e, { value }) => input.onChange(value)}
+          clearable={true}
+        />
+        {meta.error && meta.touched ? (
+          <span style={{ color: "#9F3A38" }}>{meta.error}</span>
+        ) : null}
+      </div>
+    );
+  }
+
+  renderInput({ input, meta, label }) {
+    const errorClass = meta.error && meta.touched ? "error" : null;
+    return (
+      <div className={`field ${errorClass}`}>
+        <label>{label}</label>
+        <input {...input} type="text"></input>
+        {meta.error && meta.touched ? (
+          <span style={{ color: "#9F3A38" }}>{meta.error}</span>
+        ) : null}
+      </div>
     );
   }
 
@@ -72,44 +103,44 @@ class ContactForm extends React.Component {
         onSubmit={this.props.handleSubmit(this.onFormSubmit)}
         className="ui form"
       >
-        <div className="field">
-          <label>Name</label>
-          <Field name="name" component="input" type="text" />
-        </div>
-        <div className="field">
-          <label>Role</label>
-          <Field
-            name="role"
-            component={this.renderRoleDropdown}
-            options={roleOptions}
-          />
-        </div>
-        <div className="field">
-          <label>Email</label>
-          <Field name="email" component="input" type="text" />
-        </div>
-        <div className="field">
-          <label>Phone Number</label>
-          <Field name="phone" component="input" type="text" />
-        </div>
-        <div className="field">
-          <label>What ppportunity is this related to?</label>
-          {this.props.opportunities.length === 0 ? null : (
-            <Field
-              name="opportunity"
-              component={this.renderDropdown}
-              options={this.props.opportunities}
-              disabled={this.props.initialValues ? true : false}
-            />
-          )}
-        </div>
+        <Field name="name" component={this.renderInput} label="Name" />
+        <Field
+          name="role"
+          component={this.renderRoleDropdown}
+          label="Role"
+          options={roleOptions}
+        />
+        <Field name="email" component={this.renderInput} label="Email" />
+        <Field name="phone" component={this.renderInput} label="Phone Number" />
+        <Field
+          name="opportunity"
+          component={this.renderDropdown}
+          options={this.props.opportunities}
+          disabled={this.props.initialValues ? true : false}
+          label="What opportunity is this related to?"
+        />
       </form>
     );
   }
 }
 
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.name) {
+    errors.name = "Enter a contact name";
+  }
+  if (!formValues.role) {
+    errors.role = "Enter a contact role";
+  }
+  if (!formValues.opportunity) {
+    errors.opportunity = "Specify which opportunity this is related to";
+  }
+  return errors;
+};
+
 ContactForm = reduxForm({
   form: "contactForm",
+  validate: validate,
 })(ContactForm);
 
 const mapStateToProps = (state) => {
